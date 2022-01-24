3d974a969c17760466d6c1d1b106073b749951a7
Raffaele Buscema <raffyorion93@gmail.com>
2022-01-24 12:16:24 +0100

    Modifiche

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          react_example. Esempi delle lezioni di React
         </p>
         <a
           className="App-link"
